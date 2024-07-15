c4ffebf52523bf1a906a6e6e20d6e860b2ea6530
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-15 20:35:31 +0200

    add: bottone dettaglio pizze
    ricerca By ID

--- src/main/java/com/pizzeria/controller/PizzaController.java
+++ src/main/java/com/pizzeria/controller/PizzaController.java
@@ -1,11 +1,13 @@
 package com.pizzeria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pizzeria.model.Pizza;
@@ -25,4 +27,17 @@ public class PizzaController {
 		return "index";
 	}
 
+	@GetMapping("/dettagli_pizze/{id}")
+	public String findPizzaById(@PathVariable("id") int id, Model model) {
+	    Optional<Pizza> optionalPizza = repository.findById(id);
+	    if (optionalPizza.isPresent()) {
+	        Pizza pizza = optionalPizza.get();
+	        model.addAttribute("pizza", pizza);
+	        model.addAttribute("findPizzaById", true);
+	        return "dettagli_pizze";
+	    } else {
+	        // Gestione caso in cui la pizza non è trovata (ad esempio, reindirizzamento a una pagina di errore)
+	        return "redirect:/";
+	    }
+	}
 }
