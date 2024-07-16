c8df60e04bbeb28b8e8171db79ffd5578edd6933
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-16 13:35:40 +0200

    add: Aggiunta barra di ricerca
    ora è possibile cercare la pizza per nome

--- src/main/java/com/pizzeria/controller/PizzaController.java
+++ src/main/java/com/pizzeria/controller/PizzaController.java
@@ -1,7 +1,7 @@
 package com.pizzeria.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pizzeria.model.Pizza;
 import com.pizzeria.repository.PizzaRepository;
@@ -16,7 +17,7 @@ import com.pizzeria.repository.PizzaRepository;
 @Controller
 @RequestMapping("/")
 public class PizzaController {
-	
+
 	@Autowired
 	private PizzaRepository repository;
 
@@ -28,16 +29,29 @@ public class PizzaController {
 	}
 
 	@GetMapping("/dettagli_pizze/{id}")
-	public String findPizzaById(@PathVariable("id") int id, Model model) {
-	    Optional<Pizza> optionalPizza = repository.findById(id);
-	    if (optionalPizza.isPresent()) {
-	        Pizza pizza = optionalPizza.get();
-	        model.addAttribute("pizza", pizza);
-	        model.addAttribute("findPizzaById", true);
-	        return "dettagli_pizze";
-	    } else {
-	        // Gestione caso in cui la pizza non è trovata (ad esempio, reindirizzamento a una pagina di errore)
-	        return "redirect:/";
-	    }
+	public String findPizzaById(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza = repository.getReferenceById(id);
+		if (pizza != null) {
+			model.addAttribute("pizza", pizza);
+			model.addAttribute("findPizzaById", true);
+			return "dettagli_pizze";
+		} else {
+
+			return "redirect:/";
+		}
+	}
+
+	@GetMapping("/search")
+	public String findPizzaByName(@RequestParam(name = "name", required = false) String name, Model model) {
+		List<Pizza> pizze = new ArrayList<>();
+
+		if (name == null || name.isBlank()) {
+			pizze = repository.findAll();
+
+		} else {
+			pizze = repository.findByName(name);
+		}
+		model.addAttribute("list", pizze);
+		return "index";
 	}
 }
--- src/main/java/com/pizzeria/repository/PizzaRepository.java
+++ src/main/java/com/pizzeria/repository/PizzaRepository.java
@@ -1,5 +1,7 @@
 package com.pizzeria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.pizzeria.model.Pizza;
 
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+	/* QUEARY EQUIVALENTE
+	 * 
+	 * SELECT * 
+	 * FROM book b 
+	 * WHERE p.name = ?
+	 */
+	
+	public List<Pizza> findByName(String name);
 
 }
